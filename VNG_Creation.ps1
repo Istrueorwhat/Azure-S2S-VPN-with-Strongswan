fc596008f55fc57011eb4c400cc4cc6e877889b1
Istrueorwhat <63377227+Istrueorwhat@users.noreply.github.com>
2022-10-18 22:54:52 +0200

    Create VNG_Creation.ps1

---
+++ VNG_Creation.ps1
@@ -0,0 +1,43 @@
+#Connect to Azure with powershell
+Connect-AzAccount
+# Declare variables
+  $RG = "strongSwan"
+  $Location = "North Europe"
+  $VNetName  = "VPN-vnet"
+  $FESubName = "default"
+  $VNetPrefix1 = "10.0.0.0/16"
+  $FESubPrefix = "10.0.0.0/24"
+  $GWSubPrefix = "10.0.1.0/27"
+  $GWName = "GW"
+  $GWIPName = "GWPIP"
+
+# Create a resource group
+New-AzResourceGroup -Name $RG -Location $Location
+# Create a virtual network
+$virtualNetwork = New-AzVirtualNetwork `
+  -ResourceGroupName $RG `
+  -Location $Location `
+  -Name $VNetName `
+  -AddressPrefix $VNetPrefix1
+# Create a subnet configuration
+$subnetConfig = Add-AzVirtualNetworkSubnetConfig `
+  -Name $FESubName `
+  -AddressPrefix $FESubPrefix `
+  -VirtualNetwork $virtualNetwork
+# Set the subnet configuration for the virtual network
+$virtualNetwork | Set-AzVirtualNetwork
+# Add a gateway subnet
+$vnet = Get-AzVirtualNetwork -ResourceGroupName $RG -Name $VNetName
+Add-AzVirtualNetworkSubnetConfig -Name "GatewaySubnet" -AddressPrefix $GWSubPrefix -VirtualNetwork $vnet
+# Set the subnet configuration for the virtual network
+$vnet | Set-AzVirtualNetwork
+# Request a public IP address
+$gwpip= New-AzPublicIpAddress -Name $GWIPName -ResourceGroupName $RG -Location $Location -AllocationMethod Dynamic
+# Create the gateway IP address configuration
+$vnet = Get-AzVirtualNetwork -Name $VNetName -ResourceGroupName $RG
+$subnet = Get-AzVirtualNetworkSubnetConfig -Name GatewaySubnet -VirtualNetwork $vnet
+$gwipconfig = New-AzVirtualNetworkGatewayIpConfig -Name gwipconfig1 -SubnetId $subnet.Id -PublicIpAddressId $gwpip.Id
+# Create the VPN gateway
+New-AzVirtualNetworkGateway -Name $GWName -ResourceGroupName $RG `
+-Location $Location -IpConfigurations $gwipconfig -GatewayType Vpn `
+-VpnType RouteBased -GatewaySku VpnGw2
